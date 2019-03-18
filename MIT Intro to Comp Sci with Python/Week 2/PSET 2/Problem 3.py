b1bda9b3c1d5276cbd5f448acde1c7f46a157009
Tim-Wick <tjwick77@gmail.com>
2019-03-18 14:42:19 -0700

    Finished problem

--- MIT Intro to Comp Sci with Python/Week 2/PSET 2/Problem 3.py
+++ MIT Intro to Comp Sci with Python/Week 2/PSET 2/Problem 3.py
@@ -2,11 +2,20 @@ def min_payment(balance, annualInterestRate):
     lower_payment = balance / 12
     upper_payment = (balance * (1 + annualInterestRate)**12) / 12
     end_balance = balance
-    while end_balance > 0:
+    epsilon = .01
+    payment = (lower_payment + upper_payment) / 2
+    while abs(end_balance) > epsilon:
         end_balance = balance
-        payment = (lower_payment + upper_payment / 2)
+        payment = (lower_payment + upper_payment) / 2
         for i in range(12):
             end_balance = (end_balance - payment) * (1 + (annualInterestRate/12))
-    print("Lowest Payment: {}".format(payment))
+        print(end_balance)
+        if end_balance > 0:
+            lower_payment = payment
+        else:
+            upper_payment = payment
 
-min_payment(3926, .2)
+    print("Lowest Payment: {}".format(round(payment, 2)))
+
+min_payment(320000, .2)
+min_payment(999999, .18)
